be0c8c77b6e8c204330ab3650218945442501231
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-08-05 01:29:17 -0700

    Updated

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,28 +1,89 @@
-import React from 'react'
-import Footer from '../Footer';
+import React from "react";
+import Footer from "../Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Contact() {
   return (
     <div>
-        <h1 className="Menu-Heading">Contact Us</h1>
+      <h1 className="Menu-Heading">Contact Us</h1>
 
-        <section id="contact">
-          <div className="container">
-              <div className="row">
-                <div className="col-lg-4">
-
-                </div>
-                <div className="col-lg-4">
-
-                </div>
-                <div className="col-lg-4">
-                  
-                </div>
-              </div>
+      <section id="contact">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <h3>Social Liks</h3>
+              <hr />
+              <ul>
+                <li>
+                  <a href="http://youtube.com/minhazulabedinmunna">Youtube</a>
+                </li>
+                <li>
+                  <a href="http://facebook.com/smmunna21">Facebook</a>
+                </li>
+                <li>
+                  <a href="http://twitter.com/@smmunna_703">Twitter</a>
+                </li>
+                <li>
+                  <a href="http://instragram.com/sm.munna_x">Instragram</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-4">
+              <h3>Phone</h3>
+              <hr />
+              <h6>
+                {" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-telephone"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
+                </svg>{" "}
+                +8801611765966
+              </h6>
+            </div>
+            <div className="col-lg-4">
+              <h3>Email</h3>
+              <hr />
+              <h6>
+                {" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-envelope-check-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 4.697v4.974A4.491 4.491 0 0 0 12.5 8a4.49 4.49 0 0 0-1.965.45l-.338-.207L16 4.697Z" />
+                  <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686Z" />
+                </svg>{" "}
+                minhazulabedinmunna@gmail.com
+              </h6>
+            </div>
           </div>
-        </section>
-        <Footer/>
+          <hr />
+          <div className="messageform">
+          <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Message</label>
+    <textarea type="text" class="form-control " id="exampleInputPassword1"/>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </div>
-  )
+  );
 }
