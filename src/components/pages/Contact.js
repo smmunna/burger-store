56ec67b47a36cbbc815721832a3764c29049fdac
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-08-05 01:30:11 -0700

    Updated

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -68,18 +68,35 @@ export default function Contact() {
           </div>
           <hr />
           <div className="messageform">
-          <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Message</label>
-    <textarea type="text" class="form-control " id="exampleInputPassword1"/>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">
+                  Email address
+                </label>
+                <input
+                  type="email"
+                  class="form-control "
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+                <div id="emailHelp" class="form-text">
+                  We'll never share your email with anyone else.
+                </div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">
+                  Message
+                </label>
+                <textarea
+                  type="text"
+                  class="form-control "
+                  id="exampleInputPassword1"
+                />
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Submit
+              </button>
+            </form>
           </div>
         </div>
       </section>
